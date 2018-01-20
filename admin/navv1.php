0f98181f0d0fa9f82c71776a717c5a30515ddb9e
pragatimadage <pragatimadage@gmail.com>
2018-01-20 10:35:37 +0530

    jhgfdjdhfg

--- admin/navv1.php
+++ admin/navv1.php
@@ -16,6 +16,26 @@ session_start();
                 margin-left: 90%;
             }
                
+               .btn{
+                   margin-top: 8px;
+                   margin-right: 10px;
+                   width: 200px;
+               }
+               .dropdown-menu{
+                   width: 200px;
+                  margin-right: 10px;
+                   text-align: center;
+               }
+               li a{
+                   font-family: "museo-sans", helvetica, sans-serif; 
+               }
+               .navbar-nav li a:hover, .navbar-nav li.active a{
+                    color:white
+                                        !important;
+                                    background-color: limegreen
+                                        !important;
+               }
+            
 .pagination {
     display: inline-block;
     margin-left: 30%;
@@ -39,14 +59,6 @@ session_start();
     background-color: #ddd;
     border-radius: 5px;
 }
-               .btn{
-                   margin-top: 8px;
-                   margin-right: 10px;
-               }
-               .dropdown-menu{
-                   width: 165px;
-                  margin-right: 10px;
-               }
 </style>
 </head>
 <body>
@@ -55,13 +67,13 @@ session_start();
   <div class="container-fluid">
    
 <ul class="nav navbar-nav">
-      <li class="active"><a href="navv1.php">Home</a></li>
-      <li class=""><a href="../viewuser.php">view user</a></li>
-        <li>  <a href="addemployee_1.php">Add Employee</a></li>
+      <li class="active" style="font-weight:bold;"><a  href="navv1.php">HOME</a></li>
+      <li class="" style="font-weight:bold;"><a href="../viewuser.php">VIEW USER</a></li>
+     <li style="font-weight:bold;"><a href="viewemployee.php">VIEW EMPLOYEE</a></li>
+        <li style="font-weight:bold;"><a href="addemployee_1.php">ADD EMPLOYEE</a></li>
+       
       </ul>
        <ul class="nav  navbar-right">
-      
-      
      <ul class="dropdown">
         <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['flag'];?></button> 
            <ul class="dropdown-menu">
@@ -70,116 +82,14 @@ session_start();
            </ul>
     </ul>
       </div>
-   </nav> 
-
-<div class="container">
+   </nav>
+    
+    <div class="container">
     <div class="jumbotron">
           <h1>Welcome Admin</h1>
          
   </div>
 </div>
- 
-
-    <div class="container">
-   <h2>Employee Details</h2>
-        <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "form";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error)
-{
-    die("Connection failed: " . $conn->connect_error);
- 
-}
- if(isset($_GET['page']))
-    {
-      $paa = $_GET['page'];
-
-   }
-   else
-   {    
-       $_GET['page']=1;
-//       $paa =1;
-   }
-    $paa = $_GET['page'];
-    if($paa=="" || $paa=="1")
-        {
-            $page1=0;
-        }
-   else
-    {
-      $page1=($paa*3)-3;
-    }
-$sql = "SELECT * FROM `add1` ORDER BY Address_id desc limit $page1,3";
-   $result = $conn->query($sql);
-     $sql1 = "SELECT * FROM `add1` ORDER BY Address_id desc"; 
-   $result1 = $conn->query($sql1);         
-
-mysqli_close($conn);
-
-
-?>
-
-</div>
-
-<div class="container">
-         
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Srno.</th>
-<!--        <th>ID</th>-->
-        <th>NAME</th>
-        <th>ADDRESS</th>
-        <th>CONTACT</th> 
-        <th>SALARY</th> 
-        <th>Delete</th> 
-
-      </tr>
-    </thead>
-    <tbody>
-        <?php $i=0; while($row = $result->fetch_assoc()){ ?>
-      <tr>
-          <td><?php $i++; echo $i; ?></td>
-<!--        <td><?php echo $row['id']; ?></td>-->
-        <td><?php echo $row['name']; ?></td>
-        <td><?php echo $row['address']; ?></td>
-        <td><?php echo $row['contact']; ?></td>
-        <td><?php echo $row['salary']; ?></td>
-
-          <td> <a href="dbdlt.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Do you want to delete this ? ');">Delete</a></td>        
-        </tr>
-      <?php } ?>
-    </tbody>
-  </table>
-    <div class="pagination">
-         <?php if($_GET['page'] != 1){ ?> 
-        <a href="navv1.php?page=<?=$_GET['page']-1 ?>" >prev</a>
-      <?php } ?>
-       
-    <?php
-    $count=mysqli_num_rows($result1);
-                                    
-    $p=$count/3;
-   $p=ceil($p);
-                                       
-                      for($i=1;$i<=$p;$i++)
-                                       { 
-                                           ?>
-    
-
-    <a href="navv1.php?page=<?php echo $i;?>" <?php if($_GET['page'] == $i){echo 'class="active"';} ?> ><?php echo $i;?></a><?php
-                                            
-                                       }
-                                    ?>
-       <?php if($_GET['page'] != $p){ ?>
-        <a href="navv1.php?page=<?=$_GET['page']+1 ?>">next</a>
-        <?php } ?>
-    </div>
-    </div>
-
 
 </body>
     <?php }else header("location:index.php");?>
